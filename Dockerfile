5816bd1bdb54b52a7f0e216aebc74cf35beb8f11
nishnat.rishi <nishnat.rishi@gmail.com>
2021-01-29 00:46:20 +0530

    Got postgres working!

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM python:3.8.7-alpine3.12
 WORKDIR /app
 ADD requirements.txt ./
-RUN pip install -r requirements.txt
+
+RUN \
+ apk add --no-cache postgresql-libs && \
+ apk add --no-cache --virtual .build-deps gcc musl-dev postgresql-dev && \
+ python3 -m pip install -r requirements.txt --no-cache-dir && \
+ apk --purge del .build-deps
+
 ADD ./worker_app ./
 # ENV FLASK_ENV=development
-CMD ["python", "app.py"]
+CMD ["python3", "app.py"]
--- worker_app/app.py
+++ worker_app/app.py
@@ -1,18 +1,12 @@
 import redis
-from dataclasses import dataclass
 import json
-from sqlalchemy import create_engine, Table, Column, String, MetaData
+import psycopg2
 
 r = redis.Redis(host="super-redis", port=6379)
-p = create_engine("postgresql+pg8000://postgres:postgres@super-db:5432/votes_db")
-
-print(r)
-print(p)
-
-meta = MetaData(p)
-print('Metadata created ...')
-votes_table = Table('votes', meta, Column('client_id', String), Column('vote_option', String))
-print('votes_table created ...')
+conn = psycopg2.connect("dbname='postgres' user='postgres' host='super-db' password='postgres'")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS votes(client_id text, vote_option text)")
+print("Table created ...")
 
 r.lpush("votes", json.dumps({"client_id": "Abdul", "vote_option": "Cats"}))
 r.lpush("votes", json.dumps({"client_id": "Mia", "vote_option": "Cats"}))
@@ -20,19 +14,19 @@ r.lpush("votes", json.dumps({"client_id": "Grace", "vote_option": "Dogs"}))
 
 print('Sample votes pushed to redis ...')
 
-with p.connect() as conn:
-    print("Connection established!")
-    i = 1
-    while i < 10:
-        item = r.blpop(0)
-        print("{} popped ...".format(item))
+item = r.blpop("votes", 0)
+print(item)
+vote = json.loads(item[1])
+print(vote)
 
-        vote = json.loads(item)
-        print("Converted fron JSON to {}".format(vote))
+print('Sample votes retrieved from redis ...')
 
-        conn.execute(votes_table.insert().values(client_id=vote['client_id'], vote_option=vote['vote_option']))
-        print("Successfully INSERTed into DB ...")
+cursor.execute("INSERT INTO votes VALUES(%s, %s)", (vote["client_id"], vote["vote_option"]))
 
-        i += 1
+print("INSERTed ...")
+
+conn.commit()
+
+print("Committed ...")
 
 print('Program ending ...')
