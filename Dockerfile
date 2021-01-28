c94c69263ce5709263d2e44116bd67e48a937377
nishnat.rishi <nishnat.rishi@gmail.com>
2021-01-28 22:03:44 +0530

    Changed app from Flask to normal script!

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 ADD requirements.txt ./
 RUN pip install -r requirements.txt
 ADD ./worker_app ./
-ENV FLASK_ENV=development
-CMD ["flask", "run", "--host=0.0.0.0"]
+# ENV FLASK_ENV=development
+CMD ["python", "app.py"]
--- worker_app/app.py
+++ worker_app/app.py
@@ -1,6 +1,38 @@
-from flask import Flask
-app = Flask(__name__)
+import redis
+from dataclasses import dataclass
+import json
+from sqlalchemy import create_engine, Table, Column, String, MetaData
 
-@app.route('/')
-def hello_world():
-    return 'Hell, Me!'
+r = redis.Redis(host="super-redis", port=6379)
+p = create_engine("postgresql+pg8000://postgres:postgres@super-db:5432/votes_db")
+
+print(r)
+print(p)
+
+meta = MetaData(p)
+print('Metadata created ...')
+votes_table = Table('votes', meta, Column('client_id', String), Column('vote_option', String))
+print('votes_table created ...')
+
+r.lpush("votes", json.dumps({"client_id": "Abdul", "vote_option": "Cats"}))
+r.lpush("votes", json.dumps({"client_id": "Mia", "vote_option": "Cats"}))
+r.lpush("votes", json.dumps({"client_id": "Grace", "vote_option": "Dogs"}))
+
+print('Sample votes pushed to redis ...')
+
+with p.connect() as conn:
+    print("Connection established!")
+    i = 1
+    while i < 10:
+        item = r.blpop(0)
+        print("{} popped ...".format(item))
+
+        vote = json.loads(item)
+        print("Converted fron JSON to {}".format(vote))
+
+        conn.execute(votes_table.insert().values(client_id=vote['client_id'], vote_option=vote['vote_option']))
+        print("Successfully INSERTed into DB ...")
+
+        i += 1
+
+print('Program ending ...')
